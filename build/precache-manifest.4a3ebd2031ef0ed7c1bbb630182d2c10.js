ec727b1213f3e5d680cdc072d1e184e3d85547a3
“Alexandria <afstrawter@gmail.com>
2020-03-31 18:11:57 -0500

    push

---
+++ build/precache-manifest.4a3ebd2031ef0ed7c1bbb630182d2c10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abba1e0308cb01f486ac8afc37fc6d4e",
+    "url": "/Project_Portfolio5/index.html"
+  },
+  {
+    "revision": "9a7ea1bbbaa7f9d52001",
+    "url": "/Project_Portfolio5/static/css/main.f74e5a14.chunk.css"
+  },
+  {
+    "revision": "b8ab5715cf497604b37e",
+    "url": "/Project_Portfolio5/static/js/2.20129425.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Project_Portfolio5/static/js/2.20129425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7ea1bbbaa7f9d52001",
+    "url": "/Project_Portfolio5/static/js/main.8075d78f.chunk.js"
+  },
+  {
+    "revision": "50bb11ce5256c5a978fa",
+    "url": "/Project_Portfolio5/static/js/runtime-main.120db14c.js"
+  },
+  {
+    "revision": "cc76aba740484687ba43fb2ed0ed0399",
+    "url": "/Project_Portfolio5/static/media/logo.cc76aba7.png"
+  }
+]);
